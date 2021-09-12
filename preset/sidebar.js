3de032c6375988b4f49d28b622ee5cf81034b739
ArtificialPigment <89730541+ArtificialPigment@users.noreply.github.com>
2021-09-13 01:34:36 +0800

    better

--- preset/sidebar.js
+++ preset/sidebar.js
@@ -1,17 +1,23 @@
 function PushWorkListhdl() {
-    if(document.getElementById("hdl-work-list").style.display=="none")
-    {document.getElementById("hdl-work-list").style.display="flex"}
-    else {document.getElementById("hdl-work-list").style.display="none"}
+    var hdl = document.getElementById("hdl-work-list");
+
+    if(hdl.style.display=="none")
+    {hdl.style.display="flex"}
+    else {hdl.style.display="none"}
 }
 
 function PushWorkListyx() {
-    if(document.getElementById("yx-work-list").style.display=="none")
-    {document.getElementById("yx-work-list").style.display="flex"}
-    else {document.getElementById("yx-work-list").style.display="none"}
+    var yx = document.getElementById("yx-work-list");
+
+    if(yx.style.display=="none")
+    {yx.style.display="flex"}
+    else {yx.style.display="none"}
 }
 
 function PushWorkListag() {
-    if(document.getElementById("ag-work-list").style.display=="none")
-    {document.getElementById("ag-work-list").style.display="flex"}
-    else {document.getElementById("ag-work-list").style.display="none"}
+    var ag = document.getElementById("ag-work-list");
+
+    if(ag.style.display=="none")
+    {ag.style.display="flex"}
+    else {ag.style.display="none"}
 }
