bbc35375955456337a81cf78dcfbe95defc86e7f
ArtificialPigment <89730541+ArtificialPigment@users.noreply.github.com>
2021-09-17 23:06:31 +0800

    优化js条件判定

--- preset/sidebar.js
+++ preset/sidebar.js
@@ -1,23 +1,19 @@
 function PushWorkListhdl() {
-    var hdl = document.getElementById("hdl-work-list");
-
-    if(hdl.style.display=="none")
-    {hdl.style.display="flex"}
-    else {hdl.style.display="none"}
+    var hdl = document.getElementById("hdl-work-list").style;
+    ( hdl.display == "none" ) ? hdl.display="flex" : hdl.display="none" ;
 }
 
 function PushWorkListyx() {
-    var yx = document.getElementById("yx-work-list");
-
-    if(yx.style.display=="none")
-    {yx.style.display="flex"}
-    else {yx.style.display="none"}
+    var yx = document.getElementById("yx-work-list").style;
+    ( yx.display = "none" ) ? yx.display="flex" : yx.display="none" ;
 }
 
 function PushWorkListag() {
-    var ag = document.getElementById("ag-work-list");
+    var ag = document.getElementById("ag-work-list").style;
+    ( ag.display == "none" ) ? ag.display="flex" : ag.display="none" ;
+}
 
-    if(ag.style.display=="none")
-    {ag.style.display="flex"}
-    else {ag.style.display="none"}
+function PushMore() {
+    var more = document.getElementById("more-stuff").style;
+    ( more.display == "none" ) ? more.display="flex" : more.display="none" ;
 }
